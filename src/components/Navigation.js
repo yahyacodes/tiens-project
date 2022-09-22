566feccd87c75bc43c97c7912295bf7d9af6f75e
yahya.codin <yahya.codin@gmail.com>
2022-09-22 11:07:50 +0300

    m

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,26 +6,33 @@ import image from "../images/tiens-logo-no-name-removebg-preview.png";
 const NavigationBar = () => {
   const [showNavbar, setShowNavbar] = useState(false);
 
-  const showNavbarHandler = (event) => {
+  const showNavbarHandler = () => {
     setShowNavbar(true);
   };
 
   return (
     <div>
-      <nav className={showNavbar ? classes.nav : classes.nav.expanded}>
+      <nav className={classes.nav}>
         <div className={classes.menubar} onClick={showNavbarHandler}></div>
-        <div className={classes.menubar1}></div>
-        <div className={classes.menubar2}></div>
+        <div className={classes.menubar1} onClick={showNavbarHandler}></div>
+        <div className={classes.menubar2} onClick={showNavbarHandler}></div>
+        {showNavbar && (
+          <div className={classes.closeNav}>
+            <span>X</span>
+          </div>
+        )}
         <img src={image} alt="" className={classes.image} />
         <label htmlFor="logo" className={classes.logo}>
           Tiens
         </label>
-        <ul className={classes.nav}>
-          <li>About Us</li>
-          <li>Our Products</li>
-          <li>Careers</li>
-          <li>Contact Us</li>
-        </ul>
+        {!showNavbar && (
+          <ul className={classes.nav}>
+            <li>About Us</li>
+            <li>Our Products</li>
+            <li>Careers</li>
+            <li>Contact Us</li>
+          </ul>
+        )}
       </nav>
     </div>
   );
